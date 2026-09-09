3cf0ba47851d337d68ed4828cd260c2174f2fe5f
Bakoria-Bhupendra <bakoriabhupendra@gmail.com>
2026-09-09 11:32:08 +0530

    completed the backend api

---
+++ packages/server/controllers/chat.controller.ts
@@ -0,0 +1,33 @@
+import type { Request, Response } from 'express';
+import { chatService } from '../services/chat.service';
+import z from 'zod';
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, { message: 'Prompt cannot be empty' })
+      .max(200, { message: 'Prompt cannot exceed 200 characters' }),
+   conversationId: z.string(),
+});
+
+export const chatController = {
+   async sendMessage(req: Request, res: Response) {
+      const parseResult = chatSchema.safeParse(req.body);
+      if (!parseResult.success) {
+         res.status(400).json(parseResult.error.format());
+         return;
+      }
+
+      try {
+         const { prompt, conversationId } = req.body;
+         const response = await chatService.sendMessage(prompt, conversationId);
+
+         res.json({ message: response.message });
+      } catch {
+         res.status(500).json({
+            message: 'An error occurred while processing your request.',
+         });
+      }
+   },
+};
