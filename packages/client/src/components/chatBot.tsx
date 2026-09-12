95b10da537e4f682bf21393cb276dcc5aa6b8c73
Bhupendra-Bakoria <bakoriabhupendra@gmail.com>
2026-09-12 15:07:38 +0530

    Calling the backend

--- packages/client/src/components/chatBot.tsx
+++ packages/client/src/components/chatBot.tsx
@@ -1,18 +1,54 @@
+import axios from 'axios';
+import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import { FaArrowUp } from 'react-icons/fa';
-import { Button } from './ui/button';
+
+type FormData = {
+   prompt: string;
+};
 
 const ChatBot = () => {
+   const conversationId = useRef(crypto.randomUUID());
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+   const onSubmit = async ({ prompt }: FormData) => {
+      reset();
+      const { data } = await axios.post('/api/chat', {
+         prompt,
+         conversationId: conversationId.current,
+      });
+      console.log(data);
+   };
+
+   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         e.preventDefault();
+         handleSubmit(onSubmit)();
+      }
+   };
+
    return (
-      <div className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
+      <form
+         onSubmit={handleSubmit(onSubmit)}
+         onKeyDown={onKeyDown}
+         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+      >
          <textarea
+            {...register('prompt', {
+               required: true,
+               validate: (data) => data.trim().length > 0,
+            })}
             className="w-full border-0 focus:outline-0 resize-none"
             placeholder="Ask anything..."
             maxLength={1000}
          />
-         <Button className="rounded-full w-9 h-9">
+         <button
+            disabled={!formState.isValid}
+            className="rounded-full p-2 bg-black text-white flex items-center"
+         >
             <FaArrowUp />
-         </Button>
-      </div>
+         </button>
+      </form>
    );
 };
 
